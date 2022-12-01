7b6212cb8e7e53e18e799eea3e0b3851f4cbb381
Стефан Најдовски <83938539+stefan-najdovski@users.noreply.github.com>
2022-12-01 18:22:07 +0100

    Vezba 11

--- src/me/snajdovski/vezbi6/Vezba10/UnionOfTwoLists.java
+++ src/me/snajdovski/vezbi6/Vezba10/UnionOfTwoLists.java
@@ -1,10 +1,21 @@
 package me.snajdovski.vezbi6.Vezba10;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class UnionOfTwoLists {
 
 public static void main(String[] args){
 
+String[] firstList = {"abba", "babba", "car", "door", "endek", "ford", "grandpa", "help", "insecure", "justice","harambe"};
+String[] secondList = {"ada", "bad", "cool", "door", "endek", "ford", "grandpa", "hell", "insecure", "justice","hopper"};
+    HashSet<String> union = new HashSet<>();
+    union.addAll(Arrays.asList(firstList));
+    union.addAll(Arrays.asList(secondList));
 
+    String[] unionArr = {};
+    unionArr = union.toArray(unionArr);
+    System.out.println(Arrays.toString(unionArr));
 
 }
 
